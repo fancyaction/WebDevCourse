e4bf7b3505dd15335d2dcedabaf1c9d624f1af99
fancyaction <labcoatdisaster@gmail.com>
2018-05-31 19:14:05 -0500

    Add posts.ejs

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -11,6 +11,15 @@ app.get('/fallinlovewith/:thing', (req, res) => {
   res.render('love.ejs', { thingVar: thing });
 });
 
+app.get('/posts', (req, res) => {
+  let posts = [
+    { title: 'Post 1', author: 'Susy' },
+    { title: 'My pet bunny', author: 'Charlie' },
+    { title: 'Can you believe this corgi?', author: 'Colt' }
+  ];
+  res.render('posts.ejs', { postsVar: posts });
+});
+
 app.listen(3000, () => {
   console.log('Server Start: SUCCESS');
 });
